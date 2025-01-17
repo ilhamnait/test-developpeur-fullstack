20012eaa82597111312a7c0ec24111fb1010ca26
ilhamnait <ilham.naitmbarek@gmail.com>
2025-01-17 14:42:02 -0500

    Ajout de la réponse pour la Question 2

--- question1/ProductAPI/obj/Debug/net9.0/ProductAPI.AssemblyInfo.cs
+++ question1/ProductAPI/obj/Debug/net9.0/ProductAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42611ed963e625e1e5c25888b79df55c5de1b669")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78e5541f121269a38971b1164b26532057fb1b64")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- question2/index.php
+++ question2/index.php
@@ -1,1 +1,47 @@
+<?php
+    $produits = [
+        ["id" => 1, "nom" => "Chaise", "prix" => 199.99],
+        ["id" => 2, "nom" => "Table", "prix" => 299.99],
+        ["id" => 3, "nom" => "Stylo", "prix" => 2.50]
+    ];
+
+    function produitLePlusCher($produits) {
+        // Initialisation du produit le plus cher
+        $produitCher = $produits[0];
+        
+        // Parcourir tous les produits pour trouver celui avec le prix le plus élevé
+        foreach ($produits as $produit) {
+            if ($produit['prix'] > $produitCher['prix']) {
+                $produitCher = $produit;
+            }
+        }
+        
+        return $produitCher['nom'];
+    }
+
+    function produitsMoinsChersQue($produits, $prixLimite) {
+        $resultats = [];
+        
+        // Parcourir les produits et vérifier s'ils sont moins chers que la limite
+        foreach ($produits as $produit) {
+            if ($produit['prix'] < $prixLimite) {
+                $resultats[] = $produit;
+            }
+        }
+        
+        return $resultats;
+    }
+    
+    // Test de la fonction produitLePlusCher
+    echo "Le produit le plus cher est : " . produitLePlusCher($produits) . "\n";
+
+    // Test de la fonction produitsMoinsChersQue
+    $prixLimite = 100;
+    $produitsMoinsChers = produitsMoinsChersQue($produits, $prixLimite);
+
+    echo "Produits avec un prix inférieur à $prixLimite :\n";
+    foreach ($produitsMoinsChers as $produit) {
+        echo "- " . $produit['nom'] . " (Prix: " . $produit['prix'] . ")\n";
+    }
+?>
  
